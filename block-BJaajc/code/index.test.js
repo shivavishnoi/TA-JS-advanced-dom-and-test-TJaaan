b6958a0786924ca72f9d8cb5ea793b8307840461
shivaji <shivavishnoi.vishnoi@gmail.com>
2022-09-13 20:14:31 +0530

    learning jest

--- block-BJaajc/code/index.test.js
+++ block-BJaajc/code/index.test.js
@@ -3,8 +3,16 @@ let {
   isPalindrome,
   getCircumference,
   getArea,
-} = require('./index');
+} = require("./index");
 
 test ("get full name", ()=>{
-  expect(getFullName(`shivaji`, `vishnoi`)).toBe(`shiva vishnoi`)
+  expect(getFullName(`shivaji`, `vishnoi`)).toBe(`shivaji vishnoi`)
+})
+
+test("check if Palindrome", ()=>{
+  expect(isPalindrome(`1221`)).toBe(true)
+})
+
+test("check circumference", ()=>{
+  expect(getCircumference(2)).toBe(12.56)
 })
